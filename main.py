7dae03f395b4313400a1bcec877d5774cdad7ced
updonion <87442471+updonion@users.noreply.github.com>
2021-08-06 02:43:38 +0300

    Intermediate version
    Now we can see, that all variables are correct
    OUTPUT:
    
    file_id,size,name
    https://cdn-103.anonfiles.com/0110j631n2/ee24a664-1628207299/
    File name:  [0110j631n2]_split 7.txt
    0110j631n2,52167867,"split_7_txt"
    https://cdn-108.anonfiles.com/0135gcsdn7/5ce279bb-1628207299/
    File name:  [0135gcsdn7]_1 MILLION HQ MAIL ACCESS COMBO.txt
    0135gcsdn7,28629099,"1_MILLION_HQ_MAIL_ACCESS_COMBO_txt"
    https://cdn-105.anonfiles.com/0217S94fn7/495a9067-1628207300/
    File name:  [0217S94fn7]_500K_HQ_User_Pass.txt
    0217S94fn7,12296430,"500K_HQ_User_Pass_txt"
    
    I am going to download file with name [0135gcsdn7]_1 MILLION HQ MAIL ACCESS COMBO.txt from link https://cdn-108.anonfiles.com/...

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import re
 import requests
 
 pattern = r"(https:\/\/anonfiles.com\/)([\w]{10})(\/.*)"
-search_file_id = re.compile(pattern=pattern)
+dl_name_pat = r"(https:\/\/cdn-[\d]*.anonfiles.com\/)([\w]{10})(\/[\w-]{19}\/)(.*.txt)" # TODO Works only with text files
+
+# search_file_id = re.compile(pattern=pattern)
 user_file = input("Path to your file with links: ") 
 
 rows = open(user_file, 'r')
@@ -12,4 +14,12 @@ for i in rows:
     if file_id:
         if_exist = requests.get(f"https://api.anonfiles.com/v2/file/{file_id[0][1]}/info").json()
         if if_exist['status']:
+            file_link_id = if_exist['data']['file']['metadata']['id']
+            file_page = requests.get(f"https://anonfiles.com/{file_link_id}/").text
+            download_link = re.findall(dl_name_pat, file_page)
+            ready_download_link = download_link[0][0] + download_link[0][1] + download_link[0][2]
+            print(ready_download_link)
+            file_name = f"[{file_link_id}]_{download_link[0][3]}"
+            print("File name: ", file_name)
+            # download_to = 
             print(if_exist['data']['file']['metadata']['id'], if_exist['data']['file']['metadata']['size']['bytes'], f"\"{if_exist['data']['file']['metadata']['name']}\"", sep=',')
